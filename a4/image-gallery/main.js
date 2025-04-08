59126acc66ef5135bfde6389d5c8fea2859b3d99
jh2-27 <javidan.hasanzade2@dcmail.ca>
2025-04-07 22:37:42 -0400

    Added image list and image alts

--- a4/image-gallery/main.js
+++ a4/image-gallery/main.js
@@ -10,10 +10,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const image_list = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
+const image_alts = {
+    'pic1.jpg' : 'Closeup of a human eye',
+    'pic2.jpg' : 'Rock that looks like a wave',
+    'pic3.jpg' : 'Purple and white pansies',
+    'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+    'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
