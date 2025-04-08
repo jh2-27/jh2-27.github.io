8a71249a05bc629221e9e86a1debc3b8e96d7428
jh2-27 <javidan.hasanzade2@dcmail.ca>
2025-04-07 23:09:09 -0400

    Added program loop

--- a4/object-building-practice/main.js
+++ a4/object-building-practice/main.js
@@ -92,3 +92,18 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+//Function loop method
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+//starting function loop
+loop();
