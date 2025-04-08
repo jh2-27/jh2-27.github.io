844a5d02dc31d1f474be3f0d83e3f181966ba388
jh2-27 <javidan.hasanzade2@dcmail.ca>
2025-04-07 23:10:41 -0400

    Added collision detection

--- a4/object-building-practice/main.js
+++ a4/object-building-practice/main.js
@@ -68,6 +68,23 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  //Collision detection
+  //Checks if the ball collides with any other ball
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+
 }
 
 //Array for keeping track of balls
@@ -100,6 +117,7 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
