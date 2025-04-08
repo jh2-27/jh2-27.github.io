2664a2e7b898a43f4d3acadb14a4747bc60433e0
jh2-27 <javidan.hasanzade2@dcmail.ca>
2025-04-07 23:03:09 -0400

    Added update() to Ball

--- a4/object-building-practice/main.js
+++ a4/object-building-practice/main.js
@@ -44,4 +44,28 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+
+  //update() updates ball information
+  //reverses the ball if it reached the edge of context
+  //otherwise continues moving with current x and y velocity
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
 }
